34f3694682a76ec2f54dc1fcbc93d217471a0392
hayato.saishu <h3.sbskll9@gmail.com>
2020-11-15 12:37:21 +0000

    fix2

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :posts
   validates :comment, presence: true, length: {maximum: 255 }
+  validates :name, presence: true, length: {maximum: 50 }
 end
